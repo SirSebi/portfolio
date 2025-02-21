d67a4450042261e030ce820b42a65b0b335d60e4
SirSebi <59059640+SirSebi@users.noreply.github.com>
2025-02-21 16:43:57 +0100

    update imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
-import { PointerWrapper } from '@/components/magicui/pointer'
 
 export default function Home() {
   return (
