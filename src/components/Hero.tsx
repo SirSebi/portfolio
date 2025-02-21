7df7c8bb0ec4ea93bee6f81e5b802134dd8b1cab
SirSebi <59059640+SirSebi@users.noreply.github.com>
2025-02-21 16:57:56 +0100

    remove repeatDelay

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,6 @@ export default function Hero() {
           numSquares={30}
           maxOpacity={0.1}
           duration={3}
-          repeatDelay={1}
           className={cn(
             "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
             "absolute inset-x-0 inset-y-[-50%] h-[200%] skew-y-12",
