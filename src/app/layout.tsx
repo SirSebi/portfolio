0b1a3aed49556d388cb8f2f1adf08a2b8600d9aa
SirSebi <59059640+SirSebi@users.noreply.github.com>
2025-03-05 22:17:55 +0100

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -39,7 +39,13 @@ export const metadata: Metadata = {
   },
   metadataBase: new URL("https://sebastianbrandes.com"),
   manifest: "/manifest.json",
+};
+
+export const viewport: Viewport = {
   themeColor: "#000000",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
